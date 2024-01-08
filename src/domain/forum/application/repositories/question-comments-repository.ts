f2a2055bb3954dbf782a4ec65e40714a8508ac31
Danrlei <danrleidalfre@gmail.com>
2024-01-07 22:58:16 -0300

    implement delete comment to answer and question

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,9 @@
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
 
 export interface QuestionCommentsRepository {
+  findById(id: string): Promise<QuestionComment | null>
+
   create(questionComment: QuestionComment): Promise<QuestionComment>
+
+  delete(questionComment: QuestionComment): Promise<void>
 }
