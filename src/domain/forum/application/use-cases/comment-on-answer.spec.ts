987727c0422c01269da4c21b40c53f2a1a81147e
Danrlei <danrleidalfre@gmail.com>
2024-01-07 22:41:56 -0300

    implement comment on question and answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryAnswersRepository } from '@/test/repositories/in-memory-answers-repository'
+import { makeAnswer } from '@/test/factories/make-answer'
+import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
+import { InMemoryAnswerCommentsRepository } from '@/test/repositories/in-memory-answer-comments-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment On Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment answer', async () => {
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comment on answer',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comment on answer',
+    )
+  })
+})
---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryQuestionsRepository } from '@/test/repositories/in-memory-questions-repository'
+import { makeQuestion } from '@/test/factories/make-question'
+import { InMemoryQuestionCommentsRepository } from '@/test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from '@/domain/forum/application/use-cases/comment-on-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment On Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment question', async () => {
+    const question = makeQuestion()
+
+    await inMemoryQuestionsRepository.create(question)
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Comment on question',
+    })
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Comment on question',
+    )
+  })
+})
